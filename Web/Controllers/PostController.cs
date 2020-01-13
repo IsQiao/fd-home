e5e6953a2b1c9c78150ef9c3699a371d796eb870
qinxiaowen <453000697@qq.com>
2020-01-13 14:32:25 +0800

    front end update

--- Web/Controllers/PostController.cs
+++ Web/Controllers/PostController.cs
@@ -1,22 +1,96 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Web.Managers;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Extensions;
+using Web.Models;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
-    public class PostController : Controller
+    public class PostController : BaseController
     {
-        private readonly IFileManager _fileManager;
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public PostController(IFileManager fileManager)
+        public PostController(AppDbContext dbContext, IMapper mapper)
         {
-            _fileManager = fileManager;
+            _dbContext = dbContext;
+            _mapper = mapper;
         }
 
-        [HttpGet]
-        public IActionResult Image(string image)
+        public IActionResult NewsList(PostPagerRequest viewModel)
         {
-            var mime = image.Substring(image.LastIndexOf(',') + 1);
-            return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
+            viewModel.PageSize = 5;
+            ViewBag.PagerFilter = viewModel;
+
+            var list = _dbContext
+                .Posts
+                .Where(x => x.PostType == PostType.News)
+                .Include(x => x.PostCategory)
+                .OrderByDescending(x => x.Created);
+
+            return View(_mapper.Map<IEnumerable<PostListViewModel>>(list)
+                .GetPaged(viewModel.PageNo, viewModel.PageSize));
+        }
+
+        public IActionResult NewsContent(int id)
+        {
+            var item = _dbContext.Posts
+                .Where(x => x.PostType == PostType.News)
+                .FirstOrDefault(x => x.Id == id);
+            return View(item);
+        }
+
+        public IActionResult Display()
+        {
+            var item = _dbContext.Posts.FirstOrDefault(x => x.PostType == PostType.Display);
+            return View(item);
+        }
+
+
+        public IActionResult ContactUs()
+        {
+            var item = _dbContext.Posts.FirstOrDefault(x => x.PostType == PostType.ContactUs);
+            return View(item);
+        }
+
+
+        public IActionResult AboutUs()
+        {
+            var item = _dbContext.Posts.FirstOrDefault(x => x.PostType == PostType.AboutUs);
+            return View(item);
+        }
+
+        public IActionResult Support()
+        {
+            var item = _dbContext.Posts.FirstOrDefault(x => x.PostType == PostType.Support);
+            return View(item);
+        }
+
+        public IActionResult ProductList(PostPagerRequest viewModel)
+        {
+            viewModel.PageSize = 5;
+            ViewBag.PagerFilter = viewModel;
+
+            var list = _dbContext
+                .Posts
+                .Where(x => x.PostType == PostType.Product)
+                .Include(x => x.PostCategory)
+                .OrderByDescending(x => x.Created);
+
+            return View(_mapper.Map<IEnumerable<PostListViewModel>>(list)
+                .GetPaged(viewModel.PageNo, viewModel.PageSize));
+        }
+        
+        public IActionResult ProductContent(int id)
+        {
+            var item = _dbContext.Posts
+                .Where(x => x.PostType == PostType.Product)
+                .FirstOrDefault(x => x.Id == id);
+            return View(item);
         }
     }
 }
